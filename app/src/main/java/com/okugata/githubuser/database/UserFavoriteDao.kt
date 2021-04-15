3ea119115c4d09efa4bff60c26664491d7c8c782
okugatafahmi <otasrit@gmail.com>
2021-04-16 06:16:56 +0700

    chore: change variable query

--- app/src/main/java/com/okugata/githubuser/database/UserFavoriteDao.kt
+++ app/src/main/java/com/okugata/githubuser/database/UserFavoriteDao.kt
@@ -2,17 +2,20 @@ package com.okugata.githubuser.database
 
 import android.database.Cursor
 import androidx.room.*
+import com.okugata.githubuser.database.UserFavorite.Companion.COLUMN_ID
+import com.okugata.githubuser.database.UserFavorite.Companion.COLUMN_USERNAME
+import com.okugata.githubuser.database.UserFavorite.Companion.TABLE_NAME
 import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface UserFavoriteDao {
-    @Query("SELECT * FROM ${UserFavorite.TABLE_NAME} ORDER BY username ASC")
+    @Query("SELECT * FROM $TABLE_NAME ORDER BY $COLUMN_USERNAME ASC")
     fun getAllUser(): Flow<List<UserFavorite>>
 
-    @Query("SELECT * FROM ${UserFavorite.TABLE_NAME} ORDER BY username ASC")
+    @Query("SELECT * FROM $TABLE_NAME ORDER BY $COLUMN_USERNAME ASC")
     fun getAllUserCursor(): Cursor
 
-    @Query("SELECT * FROM ${UserFavorite.TABLE_NAME} WHERE id = :id")
+    @Query("SELECT * FROM $TABLE_NAME WHERE $COLUMN_ID = :id")
     fun getUserByIdCursor(id: Long): Cursor
 
     @Insert(onConflict = OnConflictStrategy.IGNORE)
@@ -24,12 +27,12 @@ interface UserFavoriteDao {
     @Update
     fun update(userFavorite: UserFavorite): Int
 
-    @Query("DELETE FROM ${UserFavorite.TABLE_NAME}")
+    @Query("DELETE FROM $TABLE_NAME")
     suspend fun deleteAll()
 
-    @Query("DELETE FROM ${UserFavorite.TABLE_NAME} WHERE id = :id")
+    @Query("DELETE FROM $TABLE_NAME WHERE $COLUMN_ID = :id")
     fun delete(id: Long): Int
 
-    @Query("DELETE FROM ${UserFavorite.TABLE_NAME} WHERE id = :id")
+    @Query("DELETE FROM $TABLE_NAME WHERE $COLUMN_ID = :id")
     suspend fun deleteSuspend(id: Long): Int
 }
