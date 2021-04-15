8ab3c8b501fddf65a4636a1865b89a5166a567b1
okugatafahmi <otasrit@gmail.com>
2021-04-15 07:22:26 +0700

    feat: integrate notification on reminder

--- app/src/main/java/com/okugata/githubuser/activity/settings/SettingsActivity.kt
+++ app/src/main/java/com/okugata/githubuser/activity/settings/SettingsActivity.kt
@@ -1,6 +1,7 @@
 package com.okugata.githubuser.activity.settings
 
 import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.provider.Settings
 import android.view.MenuItem
@@ -8,9 +9,11 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.commit
 import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
+import androidx.preference.SwitchPreference
 import com.okugata.githubuser.R
 import com.okugata.githubuser.activity.detail.UserDetailActivity
 import com.okugata.githubuser.model.User
+import com.okugata.githubuser.reminder.ReminderReceiver
 import java.util.*
 
 class SettingsActivity : AppCompatActivity() {
@@ -36,13 +39,23 @@ class SettingsActivity : AppCompatActivity() {
         return true
     }
 
-    class SettingsFragment : PreferenceFragmentCompat() {
+    class SettingsFragment : PreferenceFragmentCompat(),
+        SharedPreferences.OnSharedPreferenceChangeListener {
+        private lateinit var reminderKey: String
+        private lateinit var reminderPreference: SwitchPreference
+        private lateinit var reminderReceiver: ReminderReceiver
+        private lateinit var time: String
+
         override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
             setPreferencesFromResource(R.xml.preferences, rootKey)
             init()
+            reminderReceiver = ReminderReceiver()
         }
 
         private fun init() {
+            reminderKey = getString(R.string.key_reminder)
+            time = getString(R.string.reminder_time)
+
             findPreference<Preference>(getString(R.string.key_developer))?.setOnPreferenceClickListener {
                 val userDetailIntent = Intent(activity, UserDetailActivity::class.java)
                 userDetailIntent.putExtra(
@@ -62,6 +75,38 @@ class SettingsActivity : AppCompatActivity() {
                     val mIntent = Intent(Settings.ACTION_LOCALE_SETTINGS)
                     startActivity(mIntent)
                     true
+                }
+            }
+
+            reminderPreference = findPreference<SwitchPreference>(reminderKey) as SwitchPreference
+            reminderPreference.apply {
+                title = getString(R.string.reminder_title, time)
+                isChecked = preferenceManager.sharedPreferences.getBoolean(reminderKey, false)
+            }
+        }
+
+        override fun onResume() {
+            super.onResume()
+            preferenceManager.sharedPreferences.registerOnSharedPreferenceChangeListener(this)
+        }
+
+        override fun onPause() {
+            super.onPause()
+            preferenceManager.sharedPreferences.unregisterOnSharedPreferenceChangeListener(this)
+        }
+
+        override fun onSharedPreferenceChanged(
+            sharedPreferences: SharedPreferences?,
+            key: String?
+        ) {
+            if (key == reminderKey) {
+                val isChecked = sharedPreferences?.getBoolean(key, false) ?: false
+                reminderPreference.isChecked = isChecked
+                if (isChecked) {
+                    reminderReceiver.setRepeatingReminder(requireContext(), time, getString(R.string.find_user))
+                }
+                else {
+                    reminderReceiver.cancelReminder(requireContext())
                 }
             }
         }
---
+++ app/src/main/java/com/okugata/githubuser/reminder/ReminderReceiver.kt
@@ -0,0 +1,121 @@
+package com.okugata.githubuser.reminder
+
+import android.app.AlarmManager
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.media.RingtoneManager
+import android.os.Build
+import android.widget.Toast
+import androidx.core.app.NotificationCompat
+
+import androidx.core.content.ContextCompat
+import com.okugata.githubuser.R
+import com.okugata.githubuser.activity.main.MainActivity
+import java.text.ParseException
+import java.text.SimpleDateFormat
+import java.util.*
+
+class ReminderReceiver : BroadcastReceiver() {
+    companion object {
+        const val TITLE_REPEATING = "Repeating Reminder"
+        const val EXTRA_MESSAGE = "message"
+
+        private const val ID_REPEATING = 100
+        private const val TIME_FORMAT = "HH:mm"
+    }
+
+    override fun onReceive(context: Context, intent: Intent) {
+        val message = intent.getStringExtra(EXTRA_MESSAGE)
+
+        if (message != null) {
+            showReminderNotification(context, message)
+        }
+    }
+
+    private fun showReminderNotification(context: Context, message: String) {
+        val intent = Intent(context, MainActivity::class.java)
+
+        val channelId = "Channel_1"
+        val channelName = "ReminderManager channel"
+        val notificationManagerCompat =
+            context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        val alarmSound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
+        val builder = NotificationCompat.Builder(context, channelId)
+            .setContentIntent(PendingIntent.getActivity(context, 0, intent, 0))
+            .setSmallIcon(R.drawable.ic_notifications_black_24dp)
+            .setContentTitle(TITLE_REPEATING)
+            .setContentText(message)
+            .setColor(ContextCompat.getColor(context, android.R.color.transparent))
+            .setVibrate(longArrayOf(1000, 1000, 1000, 1000, 1000))
+            .setSound(alarmSound)
+            .setAutoCancel(true)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                channelId,
+                channelName,
+                NotificationManager.IMPORTANCE_DEFAULT
+            )
+            channel.enableVibration(true)
+            channel.vibrationPattern = longArrayOf(1000, 1000, 1000, 1000, 1000)
+            builder.setChannelId(channelId)
+            notificationManagerCompat.createNotificationChannel(channel)
+        }
+        val notification = builder.build()
+        notificationManagerCompat.notify(ID_REPEATING, notification)
+    }
+
+    fun setRepeatingReminder(context: Context, time: String, message: String) {
+
+        // Validasi inputan waktu terlebih dahulu
+        if (isTimeInvalid(time)) return
+
+        val alarmManager = context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
+        val intent = Intent(context, ReminderReceiver::class.java).apply {
+            putExtra(EXTRA_MESSAGE, message)
+        }
+
+        val timeArray = time.split(":".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()
+
+        val calendar = Calendar.getInstance()
+        calendar.set(Calendar.HOUR_OF_DAY, Integer.parseInt(timeArray[0]))
+        calendar.set(Calendar.MINUTE, Integer.parseInt(timeArray[1]))
+        calendar.set(Calendar.SECOND, 0)
+
+        val pendingIntent = PendingIntent.getBroadcast(context, ID_REPEATING, intent, 0)
+        alarmManager.setInexactRepeating(
+            AlarmManager.RTC_WAKEUP,
+            calendar.timeInMillis,
+            AlarmManager.INTERVAL_DAY,
+            pendingIntent
+        )
+
+        Toast.makeText(context, context.getString(R.string.reminder_set, time), Toast.LENGTH_SHORT)
+            .show()
+    }
+
+    private fun isTimeInvalid(date: String): Boolean {
+        return try {
+            val df = SimpleDateFormat(TIME_FORMAT, Locale.getDefault())
+            df.isLenient = false
+            df.parse(date)
+            false
+        } catch (e: ParseException) {
+            true
+        }
+    }
+
+    fun cancelReminder(context: Context) {
+        val alarmManager = context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
+        val intent = Intent(context, ReminderReceiver::class.java)
+        val pendingIntent = PendingIntent.getBroadcast(context, ID_REPEATING, intent, 0)
+        pendingIntent.cancel()
+        alarmManager.cancel(pendingIntent)
+        Toast.makeText(context, context.getString(R.string.reminder_canceled), Toast.LENGTH_SHORT)
+            .show()
+    }
+}
